4bd046efcce04e0ad330f9b9565b5085f834d9e9
Prins Butt <prinsbutt@gmail.com>
2019-02-06 10:15:39 +0000

    Extended GUI and added callback functions for components.

--- com507-intelligent-agents/1-simulator/src/simulator/SimulatorGui.java
+++ com507-intelligent-agents/1-simulator/src/simulator/SimulatorGui.java
@@ -1,17 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package simulator;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JSlider;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
@@ -31,13 +31,8 @@ public class SimulatorGui extends JFrame {
         setVisible(true);
     }
 
-    private void initGui() {
-        setTitle(SimulatorConstants.SIM_NAME);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-    }
-
     private void initComponents() {
+        initInfo();
         initWorld();
         initControls();
     }
@@ -46,16 +41,22 @@ public class SimulatorGui extends JFrame {
         JPanel controlsPanel = new JPanel();
 
         startButton = new JButton("Start");
+        startButton.addActionListener((ActionEvent e) -> { start(); });
         controlsPanel.add(startButton);
 
         stepButton = new JButton("Step");
+        stepButton.addActionListener((ActionEvent e) -> { step(); });
         controlsPanel.add(stepButton);
 
         stopButton = new JButton("Stop");
+        stopButton.addActionListener((ActionEvent e) -> { stop(); });
         controlsPanel.add(stopButton);
 
         resetButton = new JButton("Reset");
+        resetButton.addActionListener((ActionEvent e) -> { reset(); });
         controlsPanel.add(resetButton);
+        
+        controlsPanel.add(new JSeparator(SwingConstants.VERTICAL));
 
         controlsPanel.add(new JLabel("Slow"));
 
@@ -63,6 +64,9 @@ public class SimulatorGui extends JFrame {
                 SimulatorConstants.MIN_SIM_SPEED,
                 SimulatorConstants.MAX_SIM_SPEED,
                 SimulatorConstants.INIT_SIM_SPEED);
+        speedSlider.setMinorTickSpacing(1);
+        speedSlider.setPaintTicks(true);
+        speedSlider.addChangeListener((ChangeEvent e) -> { slide(); });
         controlsPanel.add(speedSlider);
 
         controlsPanel.add(new JLabel("Fast"));
@@ -70,6 +74,24 @@ public class SimulatorGui extends JFrame {
         this.add(controlsPanel, BorderLayout.SOUTH);
     }
 
+    private void initGui() {
+        setTitle(SimulatorConstants.SIM_NAME);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+    }
+    
+    private void initInfo() {
+        JPanel infoPanel = new JPanel();
+        infoPanel.add(new JLabel("Step"));
+        
+        JTextField speedTextField = new JTextField();
+        speedTextField.setEnabled(false);
+        speedTextField.setText("" + speed);
+        infoPanel.add(speedTextField);
+        
+        add(infoPanel, BorderLayout.NORTH);
+    }
+    
     private void initWorld() {
         DefaultTableModel model = new DefaultTableModel(SimulatorConstants.WORLD_WIDTH, SimulatorConstants.WORLD_HEIGHT);
         world = new JTable(model);
@@ -85,6 +107,27 @@ public class SimulatorGui extends JFrame {
         worldPanel.add(world);
         this.add(worldPanel, BorderLayout.CENTER);
     }
+    
+    private void reset() {
+        speed = SimulatorConstants.MIN_SIM_SPEED;
+        speedSlider.setValue(speed);
+    }
+    
+    private void slide() {
+        
+    }
+    
+    private void start() {
+        
+    }
+    
+    private void step() {
+        
+    }
+    
+    private void stop() {
+        
+    }
 
     private JButton startButton;
     private JButton stepButton;
@@ -92,4 +135,6 @@ public class SimulatorGui extends JFrame {
     private JButton resetButton;
     private JSlider speedSlider;
     private JTable world;
+    
+    private int speed;
 }
