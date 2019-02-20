0f0fc44adb0e2324fffa30fd98d21575e1d25ced
Prins Butt <prinsbutt@gmail.com>
2019-02-20 06:36:43 +0000

    Updated ocean configuration class formatting.

---
+++ com507-intelligent-agents/2-ocean-simulation/2-multiple-agents/4-energy/src/prins/oceansimulation/OceanConfig.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2019 Prins Butt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package prins.oceansimulation;
+
+/**
+ *
+ * @author Prins Butt
+ */
+public class OceanConfig {
+    public static final int SEED = 28;
+    public static final double SHARK_BREEDING_PROBABILITY = 0.1;
+    public static final int SHARK_BREED_ENERGY = 2;
+    public static final int SHARK_MAX_AGE = 100;
+    public static final int SHARK_MAX_ENERGY = 100;
+    public static final int SHARK_MOVE_ENERGY = 1;
+}
