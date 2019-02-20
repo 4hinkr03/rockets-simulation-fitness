c8e5fbbc892435c2203b5b097b6427644a15d8b9
Prins Butt <prinsbutt@gmail.com>
2019-02-20 06:36:24 +0000

    Updated ocean configuration class formatting.

--- com507-intelligent-agents/2-ocean-simulation/2-multiple-agents/2-aging/src/prins/oceansimulation/OceanConfig.java
+++ com507-intelligent-agents/2-ocean-simulation/2-multiple-agents/2-aging/src/prins/oceansimulation/OceanConfig.java
@@ -22,7 +22,7 @@ package prins.oceansimulation;
  */
 public class OceanConfig {
 
+    public static final int SEED = 28;
     public static final double SHARK_BREEDING_PROBABILITY = 0.1;
     public static final int SHARK_MAXIMUM_AGE = 100;
-    public static final int SEED = 28;
 }
